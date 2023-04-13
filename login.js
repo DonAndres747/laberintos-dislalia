2bec130f3b401b2db07b4a2bcf2092d0ab1ddd44
Andres Giraldo <andres.francis46@hotmail.com>
2023-04-13 11:46:53 -0500

    segunda vista creada para el login y agregada a vista principal

---
+++ login.js
@@ -0,0 +1,8 @@
+function login(){
+    let Div = document.querySelector('div')
+    // console.log(Div)
+    // Div.setAttribute('style','background-color: red')
+    document.querySelector('body').removeChild(Div)
+    document.querySelector('a-scene').setAttribute('style', 'z-index = 1')
+
+}
