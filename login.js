0f5ad3287cfbdb8d7a2108dfce3e41aadc16551f
Andres Felipe Giraldo Bustos <andres.francis46@hotmail.com>
2023-04-22 20:46:31 -0500

    triggers arreglados cargando la logica antes que la vista

--- login.js
+++ login.js
@@ -1,4 +1,5 @@
 function login(){
+    window.parent.document.querySelector('iframe').srcdoc = 'prov.html';
     window.parent.document.querySelector('a-scene').removeAttribute('style')
     window.parent.document.querySelector('iframe').remove()
 }
